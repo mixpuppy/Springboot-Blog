f886e22bcc25f34fc4be75b27b6691ab754b1c0b
mixdog <mlv0vlm@naver.com>
2023-08-17 16:25:17 +0900

    env complet

---
+++ src/main/java/org/mixdog/tcpblog/TcpblogApplication.java
@@ -0,0 +1,13 @@
+package org.mixdog.tcpblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcpblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mixdog/tcpblog/TcpblogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mixdog.tcpblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
